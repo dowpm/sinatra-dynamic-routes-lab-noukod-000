608ffe649643aed459d94dd08bdfc39bd9dfdec1
Mac Dowell Prosper <macdowell@noukod.com>
2018-12-11 18:41:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get '/reversename/:name' do
+    "#{params[:name].reverse}"
+  end
+  
+  get '/square/:number' do
+    "#{(params[:number].to_i ** 2).to_s}"
+  end
+  
+  get '/say/:number/:phrase' do
+    "#{params[:name].reverse}"
+  end
+  
+  get '/reversename/:name' do
+    "#{params[:name].reverse}"
+  end
+  
+  get '/reversename/:name' do
+    "#{params[:name].reverse}"
+  end
 
 end
